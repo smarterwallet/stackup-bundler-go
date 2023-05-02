0b64aa972190cb2f7bf3cd9b8a568c2f022c5813
Myron <Myron.zhang@coinstore.com>
2023-05-02 14:16:19 +0800

    fix: docker build

--- build_image.sh
+++ build_image.sh
@@ -8,4 +8,4 @@ image_name="${repo_name}:${version}"
 latest="${repo_name}:latest"
 sudo docker rmi -f $(docker images -q ${repo_name})
 sudo docker build --pull -t "$latest" -t "$image_name" .
-sudo docker push "$repo_name" -a
+#sudo docker push "$repo_name" -a
