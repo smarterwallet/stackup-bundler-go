72b4ce7eefa8cd024782539c7b7bf01912aa4766
Myron <Myron.zhang@coinstore.com>
2023-05-02 14:15:14 +0800

    fix: docker build

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -3,6 +3,7 @@ package client
 
 import (
 	"errors"
+	"fmt"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -52,7 +53,9 @@ func New(
 }
 
 func (i *Client) parseEntryPointAddress(ep string) (common.Address, error) {
+	fmt.Printf("ep addr: %s\n", ep)
 	for _, addr := range i.supportedEntryPoints {
+		fmt.Printf("addr: %s\n", addr.Hex())
 		if common.HexToAddress(ep) == addr {
 			return addr, nil
 		}
